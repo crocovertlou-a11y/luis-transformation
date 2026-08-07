d4cacd3699401c2a62aa7ba0cc38de716d11c805
crocovertlou-a11y <crocovertlou@gmail.com>
2026-08-07 22:43:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='luis-build-0.6.3.1-identity';
+const CACHE='luis-build-0.6.4-ai-nutrition';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./db.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
